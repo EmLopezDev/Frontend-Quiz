a3cd36a9ad216bf9b5177dde51c0fc54a1312368
Emmanuel Lopez <emlopezdev@gmail.com>
2025-05-18 13:08:33 -0400

    added darkmode state to local storage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const bodyTag = document.getElementById("body");
 const themeCheckbox = document.getElementById("theme");
 
+let darkmode = localStorage.getItem("darkmode");
+
+const enableDarkmode = () => {
+    themeCheckbox.setAttribute("checked", true);
+    bodyTag.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkmode = () => {
+    bodyTag.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
 themeCheckbox.addEventListener("input", () => {
-    bodyTag.classList.toggle("darkmode");
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 });
